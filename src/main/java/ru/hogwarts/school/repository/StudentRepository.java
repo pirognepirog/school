cad8974c2ba5b330334a9a4bfc0625acaaaeb6ad
pirognepirog <pirognepirog@gmail.com>
2026-09-10 23:13:32 +0300

    Initial Commite

--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.hogwarts.school.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
 }
