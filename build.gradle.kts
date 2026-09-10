148a82c1bf2a1ce2d6c4a3fc1aaff08df118ab59
pirognepirog <pirognepirog@gmail.com>
2026-09-10 22:54:41 +0300

    Initial Commite

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
+    java
+}
+
+group = "ru.hogwarts"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa") // ← добавили JPA
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+
+    runtimeOnly("org.postgresql:postgresql") // ← драйвер БД (или H2)
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
